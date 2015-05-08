cfde8a1054bcfde1f7912a5f3e0ea80c0033ce53
Nathan Bracher <nrbseal@gmail.com>
2015-05-08 00:32:09 -0500

    Creates basic query to pull posts from db

--- app/routes/home.php
+++ app/routes/home.php
@@ -1,5 +1,16 @@
 <?php
 
-$app->get('/', function(){
-	echo 'Home';
-});
+$app->get('/', function() use ($app) {
+	
+	$posts = $app->db->query("
+		SELECT
+		posts.*
+		FROM posts
+		LEFT JOIN users
+		ON posts.user_id = users.id
+	")->fetchAll(PDO::FETCH_ASSOC);
+
+	var_dump($posts);
+	die();
+	
+})->name('home');
