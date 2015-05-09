d9780d9b65e47e5702e9c851a906410ca380ea9a
Nathan Bracher <nrbseal@gmail.com>
2015-05-09 12:09:49 -0500

    Concatenates first_name and last_name AS author Renders home.php template

--- app/routes/home.php
+++ app/routes/home.php
@@ -4,13 +4,15 @@ $app->get('/', function() use ($app) {
 	
 	$posts = $app->db->query("
 		SELECT
-		posts.*
+		posts.*,
+		CONCAT(users.first_name, ' ', users.last_name) AS author
 		FROM posts
 		LEFT JOIN users
 		ON posts.user_id = users.id
 	")->fetchAll(PDO::FETCH_ASSOC);
 
-	var_dump($posts);
-	die();
-	
+	$app->render('home.php', [
+		'posts' => $posts
+	]);
+
 })->name('home');
