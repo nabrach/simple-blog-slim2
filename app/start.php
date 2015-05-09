eebd60677abccd5c6a30438b8c156b6f116cd819
Nathan Bracher <nrbseal@gmail.com>
2015-05-09 12:11:32 -0500

    Adds Twig parser extension

--- app/start.php
+++ app/start.php
@@ -2,12 +2,21 @@
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim([
+	'view' => new \Slim\Views\Twig()
+]);
 
 // Database
 $app->container->singleton('db', function(){
 	return new PDO('mysql:host=localhost;dbname=slim2-blog', 'root', 'root');
 });
+
+// Views
+$view = $app->view();
+$view->setTemplatesDirectory('../app/views');
+$view->parserExtensions = [
+	new \Slim\Views\TwigExtension()
+];
 
 require 'routes.php';
 
